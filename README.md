99640befe2c407939eee39211490b3d21489f7d5
Niklas Claesson <nicke.claesson@gmail.com>
2019-04-28 14:53:00 +0200

    Move some code to example

---
+++ README.md
@@ -0,0 +1,7 @@
+# imapfetch
+
+Tool for backing up imap inboxes in mbox format.
+
+# Rationality
+
+All existing tools I found were very slow on large mbox files and I do it for educational purposes.
---
+++ examples/mbox-info.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+use imapfetch;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    path: PathBuf,
+    #[structopt(short="v", long="verbose", help="Print some more information")]
+    verbose: bool,
+}
+
+fn main() -> io::Result<()>{
+    let opt = Opt::from_args();
+    let mbox = imapfetch::Mboxfile::from_file(&opt.path)?;
+
+    let mut count;
+    if opt.verbose {
+        count = 0;
+        for entry in mbox.iter() {
+            println!("{:?}", entry);
+            count += 1;
+        }
+    } else {
+        count = mbox.iter().fold(0, |i, _| i+1);
+    }
+    println!("Found {} E-mails in mbox file", count);
+    Ok(())
+}
